420ee30b5dfefd34e3059f6ee1b2b24ef980ab69
Hyunzi Ju <jhi271@naver.com>
2015-11-03 19:27:48 +0900

    반복문 시작 #2

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-echo "Hello World, 안녕 주돼지"; //echo는 프린트
+echo "Hello World, 안녕 주부타<br />"; //echo는 프린트
+echo "<div style='color:red'>하이루~</div>";
+
+for( $i =0; $i<100; $i++){
+		echo "아하하하하하하하핳<br>";
+		?>
+		우하하<?=$i?><br>//php에서 가장 많이 사용하는 것 : 변수 출력
+		<?php
+}
+
 phpinfo(); //서버에 있는 php 정보를 모두 보여줌
 ?>
