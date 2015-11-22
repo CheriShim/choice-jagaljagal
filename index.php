4fe087e18c604bbdcd62bc69ed8b676dc672b412
Su-yeon Shim <abstract.sweets@gmail.com>
2015-11-22 17:27:22 +0900

    #7 결과 api

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ getRoute()->run();
 
 function vote(){
 	$item = array();
-	if(isset($_POST['selected_situation']) && isset($_POST['question_id']){
+	if(isset($_POST['selected_situation']) && isset($_POST['question_id'])){
 		getDatabase()->execute('SET NAMES utf8');
 		$r = getDatabase()->one( 'SELECT count(id) as cnt FROM questions where published_date = date(now()) id = '.$_POST['question_id'].' limit 1;');
 		if($r['cnt'] == 1){
